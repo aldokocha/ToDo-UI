9aa05b99dbd0244b6b9faac2fa06b77e9ef74d7d
Jae(Andrew) Kim <jae.m.kim@wsu.edu>
2020-07-19 12:58:48 -0700

    Cleaned up UI some more, and started creating the login component.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component'; // this is how to import a component
 
+// decorator
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit { // export makes it "public".
+
+  // in JS
+  message = 'Some welcome message'
+  // in Java
+  // String message = "Some welcome Message";
+  // in TS you can add type
+  // message : string = 'Some welcome message'
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { // : void means its the return type
+    console.log(this.message)
   }
 
 }
