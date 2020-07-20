2ccc10d16e333f39ce2d448cd55103ccd77fb7ac
Jae(Andrew) Kim <jae.m.kim@wsu.edu>
2020-07-19 21:40:03 -0700

    Created separate routes to separate pages for login, welcome, and error incase route was wrong. Created components for error. Learned some things about routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorComponent } from './error/error.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '**', component: ErrorComponent }
+];
 
+//<!-- Responsible for initialization of the routes -->
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,7 +9,8 @@ export class LoginComponent implements OnInit {
 
   username = 'ajmk93'
   password = ''
-
+  errorMessage = 'Invalid Credentials!'
+  invalidLogin = false;
 
   constructor() { }
 
@@ -17,8 +18,13 @@ export class LoginComponent implements OnInit {
   }
 
   handleLogin() {
+    if(this.username === "ajmk93" && this.password === "123"){
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
     console.log(this.username);
-    console.log(this.password)
+    //console.log(this.password)
   }
 
 }
